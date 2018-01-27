819507df0f3beaf3359fca0ac71b2651f60df856
mahesh asutkar <mahesh.asutkar97@gmail.com>
2018-01-27 18:33:26 +0530

    Successfully Loaded static resources

---
+++ onlineshopping/src/main/java/com/mpa/onlineshopping/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.mpa.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	
+	@RequestMapping(value={"/","/home","/index"})
+	public ModelAndView index(){
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
+		return mv;
+	}
+	
+	
+}
